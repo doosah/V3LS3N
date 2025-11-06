00195f5cdf39d89a3bf6e986e848fb1225137c4f
GitHub Action <action@github.com>
2025-11-06 02:58:34 +0000

    Auto-update deployment date [skip ci]

--- src/js/app.js
+++ src/js/app.js
@@ -799,7 +799,7 @@ function initUpdateBadge() {
     // Дата последнего обновления функционала
     // Обновляется при каждом функциональном деплое
     // Формат: DD.MM.YYYY HH:mm:ss
-    const lastUpdateDate = '06.11.2025 05:52:16';
+    const lastUpdateDate = '06.11.2025 05:58:34';
     
     // Форматируем текст плашки
     updateBadgeText.textContent = `Последнее обновление функционала ${lastUpdateDate}`;
